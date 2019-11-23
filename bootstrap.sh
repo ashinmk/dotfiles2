8991508d9d1ae7f3629e554e7bc7d51c6830fa7d
Gautham R Warrier <gauthamw@amazon.com>
2019-11-24 01:38:14 +0530

    Make rbenv lazy-load for faster prompt

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 ln -s /ws/Dotfiles/src/Gauthamw_dotfiles/micro ~/.config/micro
+ln -s /ws/Dotfiles/src/Gauthamw_dotfiles/zshrc ~/.zshrc
+ln -s /ws/Dotfiles/src/Gauthamw_dotfiles/zshprofile ~/.zshprofile
---
+++ zprofile
@@ -0,0 +1,1 @@
+/Users/gauthamw/.zprezto/runcoms/zprofile
