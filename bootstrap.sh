a1d1747afac73027d63d70fd9094124026aff84e
Gautham R Warrier <gauthamw@amazon.com>
2019-11-24 02:12:04 +0530

    Add sh suffix to z-files to help editors

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 currentDir=$(pwd);
-ln -f -s "${currentDir}/zshrc" ~/.zshrc
-ln -f -s "${currentDir}/zshprofile" ~/.zshprofile
+ln -f -s "${currentDir}/zshrc.sh" ~/.zshrc
+ln -f -s "${currentDir}/zshprofile.sh" ~/.zshprofile
 
 for initFile in $(find . -name init.sh); do
     source "$initFile";
