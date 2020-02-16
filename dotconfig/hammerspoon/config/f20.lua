90b2aa1f7df7179fa5c0723efc07945d0487a91d
Gautham R Warrier <gauthamw@amazon.com>
2020-02-17 03:46:18 +0530

    HammerSpoon: Add support for app-specific bindings for F20

--- dotconfig/hammerspoon/config/f20.lua
+++ dotconfig/hammerspoon/config/f20.lua
@@ -1,10 +1,51 @@
 local f20 = hs.hotkey.modal.new({}, nil);
+
+f20.registeredSubModals = {};
+
+function f20:registerSubModal(id, modal)
+    f20.registeredSubModals[id] = modal;
+end
+
+function f20:unregisterSubModal(id)
+    if f20.registeredSubModals[id] then
+        f20.registeredSubModals[id]:exit() -- To handle race-conditions in unregistering vs release
+        f20.registeredSubModals[id] = nil;
+    end
+end
+
+f20.appWindowFilters = {};
+
+function f20:registerAppSpecificSubModal(appName, modal)
+    appWindowFilter = hs.window.filter.new{appName};
+    appWindowFilter:subscribe(hs.window.filter.windowFocused, function()
+        f20:registerSubModal(appName, modal);
+    end)
+    appWindowFilter:subscribe(hs.window.filter.windowUnfocused, function()
+        f20:unregisterSubModal(appName);
+    end)
+    f20.appWindowFilters[appName] = appWindowFilter;
+end
+
+function f20:unregisterAppSpecificSubModal(appName)
+    if f20.appWindowFilters[appName] then
+        f20.appWindowFilters[appName]:pause();
+        f20.appWindowFilters[appName] = nil
+    end
+    f20.unregisterSubModal(appName);
+end
+
 local pressedf20 = function()
     f20:enter();
+    for _, modal in pairs(f20.registeredSubModals) do
+        modal:enter();
+    end
 end
 
 local releasedf20 = function()
     f20:exit();
+    for _, modal in pairs(f20.registeredSubModals) do
+        modal:exit();
+    end
 end
 
 hs.hotkey.bind({}, 'F20', pressedf20, releasedf20);
