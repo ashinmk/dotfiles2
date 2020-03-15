b3c579720ec8b81172fbc0fc3548d1cc39732cc0
Gautham R Warrier <gauthamw@amazon.com>
2020-03-16 00:43:01 +0530

    Fzf: Add searches for searching /Users/gauthamw and for killing processes

--- dotconfig/fzf/init.sh
+++ dotconfig/fzf/init.sh
@@ -31,7 +31,7 @@ fzf-select() {
     else
         local expected_hotkey_params="--expect=\"${expected_hotkeys}\""
     fi
-    query='echo "${input_data}" | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse $FZF_DEFAULT_OPTS $FZF_ALT_C_OPTS" fzf '${expected_hotkey_params}
+    local query='echo "${input_data}" | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse $FZF_DEFAULT_OPTS $FZF_ALT_C_OPTS" fzf '${expected_hotkey_params}
     local data="$(eval ${query})"
     local return_value=$?
     fzf_selected_text=$(head -2 <<<"$data" | tail -1)
@@ -60,10 +60,37 @@ fzf-cd-ws-widget() {
 zle -N fzf-cd-ws-widget
 bindkey '\ew' fzf-cd-ws-widget
 
-# ALT-D - cd to current dir
+# ALT-D - cd inside current dir
 fzf-cd-pwd-widget() {
     fzf-cd "fd -t d ."
     return $?
 }
 zle -N fzf-cd-pwd-widget
 bindkey '\ed' fzf-cd-pwd-widget
+
+# ALT-D - cd inside current dir
+fzf-cd-home-widget() {
+    fzf-cd "fd -t d . -p \"$HOME\""
+    return $?
+}
+zle -N fzf-cd-home-widget
+bindkey '\ec' fzf-cd-home-widget
+
+
+fzf-kill-process-widget() {
+    local selectedApp;
+    if [ "$UID" != "0" ]; then
+        selectedApp=$(ps -f -u $UID | sed 1d | awk '{print $2 " " $8}' | fzf)
+    else
+        selectedApp=$(ps -ef | sed 1d | awk '{print $2 " " $8}' | fzf)
+    fi;
+    if [[ "x$selectedApp" != "x" ]]; then
+        local pid=$(echo $selectedApp | awk '{print $1}');
+        local process=$(echo $selectedApp | awk '{print $2}');
+        echo $pid | xargs kill;
+        echo "Killed Process: ${process} with PID: ${pid}"
+    fi;
+}
+
+zle -N fzf-kill-process-widget;
+bindkey '\ek' fzf-kill-process-widget;
