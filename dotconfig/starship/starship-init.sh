2351ba03f2b5a3baef9544da15011669cc217a8e
Gautham R Warrier <gauthamw@amazon.com>
2020-10-24 14:48:28 +0530

    Ensure starship init does not override existing keymap-select fn

--- dotconfig/starship/starship-init.sh
+++ dotconfig/starship/starship-init.sh
@@ -68,10 +68,25 @@ if [[ -z ${preexec_function[(re)starship_preexec]} ]]; then
 fi
 
 # Set up a function to redraw the prompt if the user switches vi modes
-zle-keymap-select() {
+starship_zle-keymap-select() {
     starship_render
     zle reset-prompt
 }
+
+## Check for existing keymap-select widget.
+local existing_keymap_select_fn=$widgets[zle-keymap-select];
+# zle-keymap-select is a special widget so it'll be "user:fnName" or nothing. Let's get fnName only.
+existing_keymap_select_fn=${existing_keymap_select_fn//user:};
+if [[ -z ${existing_keymap_select_fn} ]]; then
+    zle -N zle-keymap-select starship_zle-keymap-select;
+else
+    # Define a wrapper fn to call the original widget fn and then Starship's.
+    starship_zle-keymap-select-wrapped() {
+        ${existing_keymap_select_fn} "$@";
+        starship_zle-keymap-select "$@";
+    }
+    zle -N zle-keymap-select starship_zle-keymap-select-wrapped;
+fi
 
 zle-line-finish() {
     export STARSHIP_CONFIG="$HOME/.config/starship.transient.toml" && starship_render && unset STARSHIP_CONFIG;
@@ -82,7 +97,6 @@ zle -N zle-line-finish;
 
 __starship_get_time && STARSHIP_START_TIME=$STARSHIP_CAPTURED_TIME
 
-zle -N zle-keymap-select
 export STARSHIP_SHELL="zsh"
 
 # Set up the session key that will be used to store logs
