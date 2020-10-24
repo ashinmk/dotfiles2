864dfb5418b9cd46bb9ec9d1edc6e1cd875eec9a
Gautham R Warrier <gauthamw@amazon.com>
2020-10-24 20:46:31 +0530

    Store nvm-lookup fn outside git repo to avoid repeated updates

--- dotconfig/nvm/init.sh
+++ dotconfig/nvm/init.sh
@@ -1,25 +1,19 @@
-my_last_known_node_version="v14.7.0";
+local node_path_helper_location="$HOME/.node-path-helper.sh";
+function setup_node_path_helper() {
+    local node_version=$(cat ~/.nvm/alias/default);
+    local node_command='export PATH="${PATH}:${HOME}/.nvm/versions/node/'$node_version'/bin"'
+    echo "$node_command" > "$node_path_helper_location"
+}
 
-node_alias_not_updated_recently=(~/.nvm/alias/default(Nm+1));
-if [[ ! -z "$node_alias_not_updated_recently" ]]; then
-    node_version_to_use="$my_last_known_node_version";
-else
-    node_version_in_alias=$(cat ~/.nvm/alias/default);
-    if [[ ! $my_last_known_node_version = $node_version_in_alias ]]; then
-        local nvm_script_path;
-        nvm_script_path="$0:A";
-        sed -i 's/'"$my_last_known_node_version"'/'"$node_version_in_alias"'/g' "$nvm_script_path";
-        echo "NVM version out of sync; Updated from $my_last_known_node_version to  $node_version_in_alias";
-    fi
-    touch -d "48 hours ago" ~/.nvm/alias/default;
-    node_version_to_use="$node_version_in_alias";
-    unset node_version_in_alias;
+[[ -f ${node_path_helper_location} ]] || setup_node_path_helper;
+
+local node_alias_not_updated_recently=(~/.nvm/alias/default(Nm+1));
+if [[ -z "$node_alias_not_updated_recently" ]]; then
+    setup_node_path_helper;
+    touch -d "48 hours ago" ~/.nvm/alias/default; 
 fi;
-unset node_alias_not_updated_recently;
-unset my_last_known_node_version;
 
-export PATH="${PATH}:${HOME}/.nvm/versions/node/$node_version_to_use/bin"
-unset node_version_to_use;
+source "$node_path_helper_location"; 
 
 nvm() {
     export NVM_DIR="$HOME/.nvm"
