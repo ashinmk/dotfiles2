a5b9325878a765ebbfd0dfb716fece056b7abb16
Gautham R Warrier <gauthamw@amazon.com>
2020-11-07 15:21:43 +0530

    Execute zsh keybind commands from line-buffer

--- dotconfig/git/init.sh
+++ dotconfig/git/init.sh
@@ -12,7 +12,6 @@ fzf-git-change-branch() {
         fi;
         zle accept-line;
     fi
-    zle reset-prompt;
 }
 
 zle -N fzf-git-change-branch;
@@ -21,25 +20,24 @@ bindkey '\egb' fzf-git-change-branch;
 fzf-git-change-branch-remote() {
     local selectedBranch;
     selectedBranch=$(git br -r | tr -d '*' | tr -d '[:blank:]' | fzf --preview 'git hmm --color {}'  --preview-window right:70%);
-    [[ -z "$selectedBranch" ]] || git co "$selectedBranch";
+    [[ -z "$selectedBranch" ]] || BUFFER="git co \"${selectedBranch}\"" && zle accept-line;
     zle reset-prompt;
 }
 
 zle -N fzf-git-change-branch-remote;
 bindkey '\egB' fzf-git-change-branch-remote;
 
-show-git-status() {
-    git status;
-    zle reset-prompt;
+zle-show-git-status() {
+    BUFFER="git status";
+    zle accept-line;
 }
 
-zle -N show-git-status;
-bindkey '\egs' show-git-status;
+zle -N zle-show-git-status;
+bindkey '\egs' zle-show-git-status;
 
 fzf-git-rebase-interactive() {
     targetCommit=$(git hmm --color | fzf --ansi | cut -d '[' -f 2 | cut -d ']' -f 1);
-    [[ -z "$targetCommit" ]] || git rebase -i "$targetCommit~1";
-    zle reset-prompt;
+    [[ -z "$targetCommit" ]] || BUFFER="git rebase -i \"${targetCommit}~1\"" && zle accept-line;
 }
 
 zle -N fzf-git-rebase-interactive;
@@ -48,8 +46,7 @@ bindkey '\egr' fzf-git-rebase-interactive;
 fzf-git-diff() {
     local selectedFile;
     selectedFile=$(git diff --name-only | fzf --preview 'git diff {}');
-    [[ -z "$selectedFile" ]] || git diff "$selectedFile";
-    zle reset-prompt;
+    [[ -z "$selectedFile" ]] || BUFFER="git diff \"${selectedFile}\"" && zle accept-line;
 }
 
 zle -N fzf-git-diff;
