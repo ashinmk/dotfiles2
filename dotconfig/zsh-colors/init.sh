da4fc934532008a464ac4d947a16c29f48953424
Gautham R Warrier <gauthamw@amazon.com>
2020-12-27 11:27:04 +0530

    Change zsh suggestion color to match Tomorrow Night

---
+++ dotconfig/zsh-colors/init.sh
@@ -0,0 +1,3 @@
+# Set Suggestion Color for "Tomorrow Night" theme.
+# Ref https://upload.wikimedia.org/wikipedia/commons/1/15/Xterm_256color_chart.svg for colors
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=246'
