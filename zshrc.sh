13aa01966d292f1af6bef9e8f13c7c6505a1212b
Gautham R Warrier <gauthamw@amazon.com>
2021-01-01 17:48:33 +0530

    Add zsh profiling code for when needed

--- zshrc.sh
+++ zshrc.sh
@@ -1,3 +1,6 @@
+# Uncomment to benchmark load-time
+#zmodload zsh/zprof
+
 export PATH="${HOME}/Scripts/bin:${HOME}/.toolbox/bin:$PATH"
 
 source "$HOME/.dotconfig/early-env-setup/zsh-opts.sh";
@@ -27,3 +30,6 @@ done
 zinit light-mode for pick="powerlevel10k.zsh-theme" src="$HOME/.dotconfig/p10k/p10k.zsh" depth="1" romkatv/powerlevel10k;
 
 zinit wait lucid light-mode for id-as="lazy-init" pick="lazy-init.sh" "${HOME}/.dotconfig";
+
+# Uncomment to benchmark load-time
+#zprof
