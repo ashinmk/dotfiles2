31a6edc2c5dd84271fb57f1ee3c33393f55e7e62
Gautham R Warrier <gauthamw@amazon.com>
2021-01-01 19:28:32 +0530

    Hammerspoon: Make Hammerspoon Management changes bound to hyper-key

--- dotconfig/hammerspoon/config/f20_general.lua
+++ dotconfig/hammerspoon/config/f20_general.lua
@@ -1,14 +1,14 @@
 -- Reload HammerSpoon Config
-f20:bind({'cmd', 'shift'}, '[', function()
+f20:bind({'cmd', 'shift', 'alt', 'ctrl'}, '[', function()
     hs.reload();
 end)
 
 -- Clear HammerSpoon Console
-f20:bind({'cmd', 'shift'}, ']', function()
+f20:bind({'cmd', 'shift', 'alt', 'ctrl'}, ']', function()
     hs.console.clearConsole();
 end)
 
-f20:bind({'cmd', 'shift'}, '\\', function()
+f20:bind({'cmd', 'shift', 'alt', 'ctrl'}, '\\', function()
     local appInfo = "Name:  " .. hs.application.frontmostApplication():name();
     appInfo = appInfo .. '\n' .. "BundleId:  " .. hs.application.frontmostApplication():bundleID();
     hs.pasteboard.setContents(appInfo);
