46621bbc666829eea0bc1fcbc2afdd8ef0167379
Gautham R Warrier <gauthamw@amazon.com>
2021-01-03 14:50:33 +0530

    Fix bootstrap creating invalid Amazon-ext dotconfig

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,8 @@ touch "$HOME/.hushlogin"
 rm -rf ~/.dotconfig;
 ln -f -s "${currentDir}/dotconfig" ~/.dotconfig;
 rm -rf ~/.amazon.ext.dotconfig
-ln -f -s "${currentDir}/amazon.ext.dotconfig" ~/.amazon.ext.dotconfig;
+mkdir -p ~/.amazon.ext.dotconfig/sample;
+touch ~/.amazon.ext.dotconfig/sample/init.sh
 
 for bootstrapFile in $(find -L dotconfig -name bootstrap.sh); do
     source "$bootstrapFile";
