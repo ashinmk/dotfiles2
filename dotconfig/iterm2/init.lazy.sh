3651a0fa0a549b73aea4a242e8d19d9dd1fe40c4
Gautham R Warrier <gauthamw@amazon.com>
2021-01-03 14:58:49 +0530

    Make dedicated lazy-init file for iTerm2 shell integration

---
+++ dotconfig/iterm2/init.lazy.sh
@@ -0,0 +1,1 @@
+zinit wait lucid for id-as="iterm2-shell-integration" pick=".iterm2_shell_integration.zsh" "${HOME}";
--- dotconfig/lazy-init.sh
+++ dotconfig/lazy-init.sh
@@ -3,8 +3,6 @@ zinit wait lucid light-mode for \
     atload='_zsh_autosuggest_start' zsh-users/zsh-autosuggestions \
     blockf atpull='zinit creinstall -q .' zsh-users/zsh-completions
 
-zinit wait lucid for id-as="iterm2-shell-integration" pick=".iterm2_shell_integration.zsh" "${HOME}";
-
 for lazyInitFile in $HOME/.dotconfig/*/init.lazy.sh; do
     local fileDir=${${lazyInitFile%/*}##*/};
     zinit wait lucid light-mode for id-as="fileDir" nocd pick="$lazyInitFile" "${HOME}/.dotconfig/$fileDir";
