b03e5a98e75d2d7ceb5528ce021e879b346d7477
Gautham R Warrier <gauthamw@amazon.com>
2021-01-30 19:03:24 +0530

    Fix typo in keyboard layout script

--- bootstrap/setup-keyboard-layout.sh
+++ bootstrap/setup-keyboard-layout.sh
@@ -1,4 +1,4 @@
 #!/usr/local/bin/zsh
 
 cp x_layout.keylayout $HOME/Library/Keyboard\ Layouts/x_layout.keylayout
-echo 'Logout & Login and then change Input Source in Settings to x_layout. It'll show up in "Others"'
+echo 'Logout & Login and then change Input Source in Settings to x_layout. It\'ll show up in "Others"'
