0cb8223794d5924557eb38c9ff87d892ed54d344
Gautham R Warrier <gauthamw@amazon.com>
2021-02-06 16:09:36 +0530

    Add zle binding for ripgrep+fzf

---
+++ dotconfig/ripgrep/init.sh
@@ -0,0 +1,16 @@
+function search_with_fzf_rg() {
+    local INITIAL_QUERY=""
+    local RG_PREFIX="rg --column --line-number --no-heading --color=always --smart-case"
+    FZF_DEFAULT_COMMAND="$RG_PREFIX '$INITIAL_QUERY'" \
+    fzf --bind "change:reload:$RG_PREFIX {q} || true" \
+        --ansi --disabled --query "$INITIAL_QUERY" \
+        --height=50% --layout=reverse;
+}
+
+function zle_search_with_fzf_rg() {
+    search_with_fzf_rg;
+    echo "";    # Needed since we use instant-prompt that clears the immediate preceeding line;
+}
+
+zle -N zle_search_with_fzf_rg;
+bindkey '\ef' zle_search_with_fzf_rg;
