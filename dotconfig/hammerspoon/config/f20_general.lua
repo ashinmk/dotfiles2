650123c7e619717fbb5e2aa5a5788d45ec025198
Gautham R Warrier <gauthamw@amazon.com>
2021-02-08 09:51:42 +0530

    Hammerspoon: Revert f20 general bindings for reload/clear console

--- dotconfig/hammerspoon/config/f20_general.lua
+++ dotconfig/hammerspoon/config/f20_general.lua
@@ -1,14 +1,14 @@
 -- Reload HammerSpoon Config
-f20:bind({'cmd', 'shift', 'alt', 'ctrl'}, '[', function()
+f20:bind({}, '[', function()
     hs.reload();
 end)
 
 -- Clear HammerSpoon Console
-f20:bind({'cmd', 'shift', 'alt', 'ctrl'}, ']', function()
+f20:bind({}, ']', function()
     hs.console.clearConsole();
 end)
 
-f20:bind({'cmd', 'shift', 'alt', 'ctrl'}, '\\', function()
+f20:bind({}, '\\', function()
     local appInfo = "Name:  " .. hs.application.frontmostApplication():name();
     appInfo = appInfo .. '\n' .. "BundleId:  " .. hs.application.frontmostApplication():bundleID();
     hs.pasteboard.setContents(appInfo);
