3f2d6a9c60fb84df88a3025ac02951ae056b1926
Gautham R Warrier <gauthamw@amazon.com>
2021-05-08 11:25:49 +0530

    Remove unnecessary rsync in auth

--- dotconfig/amazon/aliases.sh
+++ dotconfig/amazon/aliases.sh
@@ -8,7 +8,6 @@ function auth() {
     else
         kinit.exp gauthamw && mwinit.exp gauthamw && (ssh-add > /dev/null) && touch "/tmp/.recently-authorized";
         (rsync -aquv ~/.midway gauthamw.aka.corp.amazon.com:/home/gauthamw/ &) > /dev/null;
-        (rsync -aquv /tmp/.recently-authorized gauthamw.aka.corp.amazon.com:/home/gauthamw/ &) > /dev/null;
         (print_password | (ssh -q gauthamw.aka.corp.amazon.com "kinit -f -l 86400  && touch \"/tmp/.recently-authorized\"" > /dev/null) &) > /dev/null;
     fi;
 }
