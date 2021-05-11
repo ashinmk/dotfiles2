b60b8f87d2599a95995689b9f027bd1084d68088
Gautham R Warrier <gauthamw@amazon.com>
2021-05-11 11:24:59 +0530

    Splitting git diff view horizontally

--- dotconfig/git/init.sh
+++ dotconfig/git/init.sh
@@ -1,4 +1,9 @@
+is_in_git_repo() {
+  git rev-parse HEAD > /dev/null 2>&1
+}
+
 fzf-git-change-branch() {
+    is_in_git_repo || return
     local out;
     out=$(git br | tr -d '*' | tr -d '[:blank:]' | fzf --preview 'git hmm --color {}'  --preview-window right:70% --expect=alt-d);
     local selectedBranch altCommand;
@@ -19,6 +24,7 @@ zle -N fzf-git-change-branch;
 bindkey '\egb' fzf-git-change-branch;
 
 fzf-git-change-branch-remote() {
+    is_in_git_repo || return
     local selectedBranch;
     selectedBranch=$(git br -r | tr -d '*' | tr -d '[:blank:]' | fzf --preview 'git hmm --color {}'  --preview-window right:70%);
     [[ -z "$selectedBranch" ]] || BUFFER="git co \"${selectedBranch}\"" && zle autosuggest-clear && zle accept-line;
@@ -29,6 +35,7 @@ zle -N fzf-git-change-branch-remote;
 bindkey '\egB' fzf-git-change-branch-remote;
 
 zle-show-git-status() {
+    is_in_git_repo || return
     BUFFER="git status";
     zle accept-line;
 }
@@ -37,6 +44,7 @@ zle -N zle-show-git-status;
 bindkey '\egs' zle-show-git-status;
 
 fzf-git-rebase-interactive() {
+    is_in_git_repo || return
     targetCommit=$(git hmm --color | fzf --ansi | cut -d '[' -f 2 | cut -d ']' -f 1);
     [[ -z "$targetCommit" ]] || BUFFER="git rebase -i \"${targetCommit}~1\"";
     zle autosuggest-clear;
@@ -47,9 +55,10 @@ zle -N fzf-git-rebase-interactive;
 bindkey '\egr' fzf-git-rebase-interactive;
 
 fzf-git-diff() {
+    is_in_git_repo || return
     local selectedFile;
     local gitRootDir=$(git rev-parse --show-toplevel);
-    selectedFile=$(git diff --name-only | fzf --preview 'git diff --color '${gitRootDir}'/{}');
+    selectedFile=$(git diff --name-only | fzf --preview-window top:80%,border-horizontal --preview 'git diff --color '${gitRootDir}'/{}');
     [[ -z "$selectedFile" ]] || BUFFER="git diff \"${gitRootDir}/${selectedFile}\"";
     zle autosuggest-clear && zle accept-line;
 }
@@ -59,6 +68,7 @@ bindkey '\egd' fzf-git-diff;
 
 
 zle-git-hmm() {
+    is_in_git_repo || return
     BUFFER="git hmm";
     zle accept-line;
 }
@@ -67,6 +77,7 @@ zle -N zle-git-hmm;
 bindkey '\egh' zle-git-hmm;
 
 fzf-git-add() {
+    is_in_git_repo || return
     local gitRootDir=$(git rev-parse --show-toplevel);
     local modifiedFiles=$(git diff --name-only);
     local untrackedPaths=$(git ls-files $gitRootDir --exclude-standard --others --full-name);
@@ -82,6 +93,7 @@ zle -N fzf-git-add;
 bindkey '\ega' fzf-git-add;
 
 fzf-git-unstage() {
+    is_in_git_repo || return
     local gitRootDir=$(git rev-parse --show-toplevel);
     local selectedFiles=$(git diff --cached --name-only | fzf -m --reverse --preview 'git diff --color '${gitRootDir}'/{}');
     if [[ ! -z "$selectedFiles" ]]; then
@@ -95,6 +107,7 @@ zle -N fzf-git-unstage;
 bindkey '\egA' fzf-git-unstage;
 
 zle-git-pull() {
+    is_in_git_repo || return
     BUFFER="git pull";
     zle accept-line;
 }
@@ -103,6 +116,7 @@ zle -N zle-git-pull;
 bindkey '\egp' zle-git-pull;
 
 zle-git-clean() {
+    is_in_git_repo || return
     BUFFER="git clean -df";
     zle accept-line;
 }
